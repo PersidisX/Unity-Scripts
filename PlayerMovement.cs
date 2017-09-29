13b1fdb6b5bd75bb4e24a81ab40b5b3e93d1bc8a
PersidisX <32102905+PersidisX@users.noreply.github.com>
2017-09-29 18:29:33 +0300

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb;
+    private int count;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        count = 0;
+        countText.text = "";
+        winText.text = "";
+        SetCountText();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count" + count.ToString();
+        if (count >= 11)
+            //... then set the text property of our winText object to "You win!"
+            winText.text = "You win!";
+    }
+}
